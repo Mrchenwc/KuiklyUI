dafac58bdac7410c98df7dfb1e9a4b2546087052
Mrchenwc <547340423@qq.com>
2025-09-01 20:12:55 +0800

    Update KRImageView.m

--- core-render-ios/Extension/Components/KRImageView.m
+++ core-render-ios/Extension/Components/KRImageView.m
@@ -504,6 +504,7 @@ typedef void (^KRSetImageBlock) (UIImage *_Nullable image);
 
 - (void)setHr_rootView:(KuiklyRenderView *)hr_rootView {
     _imageView.hr_rootView = hr_rootView;
+    _placeholderView.hr_rootView = hr_rootView;
 }
 
 #pragma mark - KuiklyRenderViewExportProtocol
@@ -544,7 +545,8 @@ typedef void (^KRSetImageBlock) (UIImage *_Nullable image);
         [_placeholderView removeFromSuperview];
         if (_css_placeholder.length) {
             _placeholderView = [[KRImageView alloc] initWithFrame:self.bounds];
-            _placeholderView.contentMode = _imageView.contentMode;
+            _placeholderView.contentMode = _imageView.contentMode；
+            _placeholderView.hr_rootView = _imageView.hr_rootView；
             _placeholderView.css_src = css_placeholder;
             [self insertSubview:_placeholderView atIndex:0];
         } else {
